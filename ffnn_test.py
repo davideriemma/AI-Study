6f12981d92f26d99518da4d1fa6ec6146a01dfce
Davide Riemma <riemma.d@gmail.com>
2021-06-02 13:20:46 +0200

    created a draft of the test neural network

--- ffnn_test.py
+++ ffnn_test.py
@@ -20,3 +20,13 @@ layer3 = f.FFNN_Layer(0, 2, layer2.number_of_neurons())
 
 print("is there an tree?")
 print(layer3(layer2(layer1(test_data)))) #determine if there is a tree
+
+class FFNN_rec_trees:
+
+    def __init__(self):
+        self.__layer1__ = f.FFNN_Layer(720*720, 0) #input layer
+        self.__layer2__ = f.FFNN_Layer(2000, self.__layer1__.get_number_of_neurons()) #riconosce i tratti elementari
+                                                                                      #che compongono un albero (linee
+                                                                                      #curve, linee dritte, etc..)
+        self.__layer3__ = f.FFNN_Layer(10, self.__layer2__.get_number_of_neurons()) #riconosce la chioma, il tronco, le radici, etc..
+        self.__layer4__ = f.FFNN_Layer(2, self.__layer3__.get_number_of_neurons()) #recognises wheather the image shown contains a tree or not
